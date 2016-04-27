d40f3226ff1340e05c57428fb2a589c72ed61614
Albert <aenyedy56@gmail.com>
2016-04-27 15:49:39 -0400

    Added fields and lorem ipsum methods to THRM_HeatingPads

--- THRM_HeatingPads.java
+++ THRM_HeatingPads.java
@@ -1,14 +1,28 @@
 
 public class THRM_HeatingPads extends Module implements THRM{
 	
+	double inputVoltage;
+	double outputVoltage;
+	
 	public THRM_HeatingPads(double mass, double cost, String protocol, double powerCapacity){
 		super(mass, cost, protocol, powerCapacity);
+		this.inputVoltage = 0;
+		this.outputVoltage = 0;
+	}
+	
+	public void setInputVoltage(double input){
+		this.inputVoltage = input;
+	}
+	
+	public double getSetOutVoltage(){
+		this.outputVoltage = 1; //insert equation mapping input voltage to output voltage
+		return this.outputVoltage;
 	}
 	
 	public double get_thermal_reading() {
 		// TODO Auto-generated method stub
 		
-		//this would return the parts thermal reading
+		//this would return the thermistor's thermal reading
 		
 		return 0;
 	}
